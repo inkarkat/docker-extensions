4fd4d15e0cc63db6c1c6fa96531e52b7313ff755
Ingo Karkat <swdev@ingo-karkat.de>
2023-02-16 09:10:02 +0100

    Define docker-wrapper and just delegate to it in the docker() wrapper function
    To allow for more flexibility and that other Docker command extensions can transparently invoke native commands or extensions.
    Modeled after my kubectl-wrapper.

--- shell/wrappers.sh
+++ shell/wrappers.sh
@@ -1,16 +1,6 @@
 #!/bin/sh source-this-script
 
-# Allow definition of Docker aliases by putting an executable "docker-foo"
-# somewhere in the PATH.
 docker()
 {
-    typeset dockerAlias="docker-$1"
-    if [ $# -eq 0 ]; then
-	docker ${DOCKER_DEFAULT_COMMAND:-ps}
-    elif type ${BASH_VERSION:+-t} "$dockerAlias" >/dev/null 2>&1; then
-	shift
-	$dockerAlias "$@"
-    else
-	command docker "$@"
-    fi
+    docker-wrapper "$@"
 }
